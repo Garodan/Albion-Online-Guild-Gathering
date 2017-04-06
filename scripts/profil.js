0d326574e0a9feded00c0a294a217cd680155a7c
Jamie Schenker <s_schenker@stud.hwr-berlin.de>
2017-04-06 22:02:47 +0200

    darstellungsfehler behoben

--- scripts/profil.js
+++ scripts/profil.js
@@ -7,7 +7,7 @@ function entryfield() {
     var div = document.createElement("div");
 	div.setAttribute("class", "form-group removeitem"+i);
 	var rdiv = 'removeitem'+i;
-    div.innerHTML = '<div class="col-sm-3 nopadding"><div class="form-group"> <input type="text" class="form-control" id="item" name="item[]" value="" placeholder="Item name"></div></div><div class="col-sm-3 nopadding"><div class="form-group"> <input type="text" class="form-control" id="quantitiy" name="quantitiy[]" value="" placeholder="Anzahl/ quantitiy"></div></div><div class="col-sm-3 nopadding"><div class="form-group"><div class="input-group"> <div class="input-group-btn"> <button class="btn btn-danger" type="button" onclick="remove_entryfield('+ i +');"> <span class="glyphicon glyphicon-minus" aria-hidden="true"></span> </button></div></div></div></div><div class="clear"></div>';
+    div.innerHTML = '<div class="col-sm-3 nopadding"><div class="form-group"> <input type="text" class="form-control" id="item" name="item[]" value="" placeholder="Item name"></div></div><div class="col-sm-3 nopadding"><div class="form-group"> <input type="text" class="form-control" id="quantitiy" name="quantitiy[]" value="" placeholder="Anzahl/ quantitiy"></div></div><div class="col-sm-3 nopadding"><div class="form-group"><div class="input-group"> <div class="input-group-btn"> <button class="btn btn-danger" type="button" onclick="remove_entryfield('+ i +');"> <span class="glyphicon glyphicon-minus" aria-hidden="true"></span> </button></div></div></div></div><div class="clear"></div><div class="col-sm-6 nopadding"><div class="form-group"></div></div>';
 
     objTo.appendChild(div)
 }
