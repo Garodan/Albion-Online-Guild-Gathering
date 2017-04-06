9d0b4bcd77af9262a5ce8792008d834afd329245
Garodan <can.h.kalkan@gmail.com>
2017-04-06 19:29:13 +0200

    Überarbeitung

--- app.js
+++
@@ -1,6 +0,0 @@
-var app = angular.module('guildsApp',[])
-  app.controller('NewsCtrl', function($scope, $http){
-    $http.get('news.json').then(function(newsResponse) {
-      $scope.news = newsResponse.data;
-    });
-  });
