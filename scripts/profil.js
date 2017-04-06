63de929e7815b06a5c293617d16e1686ad5e9f25
Garodan <can.h.kalkan@gmail.com>
2017-04-06 20:17:13 +0200

    Hotfix
    tut schon mal was es soll!

--- scripts/profil.js
+++ scripts/profil.js
@@ -7,15 +7,10 @@ function education_fields() {
     var divtest = document.createElement("div");
 	divtest.setAttribute("class", "form-group removeclass"+room);
 	var rdiv = 'removeclass'+room;
-    divtest.innerHTML = '<div class="col-sm-3 nopadding"><div class="form-group"><input type="text" class="form-control" id="item" name="item[]" value="" placeholder="Item name"></div></div><div class="col-sm-3 nopadding"><div class="form-group"><input type="text" class="form-control" id="quantitiy" name="quantitiy[]" value="" placeholder="Anzahl/ quantitiy"></div></div><div class="col-sm-3 nopadding"><div class="form-group"><div class="input-group"> <div class="input-group-btn"><button class="btn btn-danger" type="button" onclick="remove_education_fields('+ room +');"> <span class="glyphicon glyphicon-minus" aria-hidden="true"></span></button></div></div></div></div><div class="clear"></div>';
+    divtest.innerHTML = '<div class="col-sm-3 nopadding"><div class="form-group"> <input type="text" class="form-control" id="item" name="item[]" value="" placeholder="Item name"></div></div><div class="col-sm-3 nopadding"><div class="form-group"> <input type="text" class="form-control" id="quantitiy" name="quantitiy[]" value="" placeholder="Anzahl/ quantitiy"></div></div><div class="col-sm-3 nopadding"><div class="form-group"><div class="input-group"> <div class="input-group-btn"> <button class="btn btn-danger" type="button" onclick="remove_education_fields('+ room +');"> <span class="glyphicon glyphicon-minus" aria-hidden="true"></span> </button></div></div></div></div><div class="clear"></div>';
 
     objTo.appendChild(divtest)
 }
    function remove_education_fields(rid) {
 	   $('.removeclass'+rid).remove();
    }
-
-   <div class="input-group-btn">
-   <button class="btn btn-danger" type="button" onclick="remove_education_fields('+ room +');"> <span class="glyphicon glyphicon-minus" aria-hidden="true"></span>
-   </button>
-   </div>
